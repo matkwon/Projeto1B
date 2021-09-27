0ee2985fe9bbbc212faa3092b1caf602a07b5bab
matkwon <kwonmat@outlook.com.br>
2021-09-27 14:29:28 -0300

    CRUD para filtro

--- notes/urls.py
+++ notes/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('update', views.update, name='update'),
     path('tags', views.tags, name='tags'),
     path('filter', views.tags, name='filter'),
+    path('filter_post', views.filter_post, name='filter_post'),
+    path('filter_delete', views.filter_delete, name='filter_delete'),
+    path('filter_update', views.filter_update, name='filter_update'),
 ]
